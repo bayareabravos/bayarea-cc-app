4cbc224deb6ef6463b841285a50833d553d513ad
Jayasree Vangal <jayasree.vangal@kp.org>
2020-04-30 15:33:16 -0700

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Bay Area CC
+          Welcome to Bay Area Cricket Club
         </p>
         <a
           className="App-link"
