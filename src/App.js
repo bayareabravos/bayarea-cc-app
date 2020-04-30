862aa7248ae17c14cc6ba28a5c7929270d9fc771
Karthik <Karthik.Rayala@kp.org>
2020-04-29 23:08:55 -0700

    Initial Changes for BayArea CC app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './bayareacc-logo.png';
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Bay Area CC
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://cricclubs.com/NCCA/viewInternalClub.do?internalClubId=14&clubId=1191"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Site Under Construction! Please visit our club page in CricClubs for now.
         </a>
       </header>
     </div>
